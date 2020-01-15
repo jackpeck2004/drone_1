f1863b35246eed206b41ea547517ce08375cbf56
Giacomo <giacomo.pasin@gmail.coom>
2020-01-15 07:32:09 +0100

    created drone class

--- tello.py
+++ tello.py
@@ -4,15 +4,26 @@ import socket
 import keys
 import time
 
-message = "command"
+class Tello:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.socket = socket.socket(socket.AD_INET, socket.SOCK_DGRAM)
+        message = "command"
+        sock.sendto(message.encode('utf-8'), (keys.UDP_IP, keys.UDP_PORT))
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        time.sleep(2)
 
-sock.sendto(message.encode('utf-8'), (keys.UDP_IP, keys.UDP_PORT))
 
-time.sleep(2)
+    def send_command(self, command):
+        self.socket.sendto(command.encode('utf-8'), (self.ip, self.port))
 
-message = "takeoff"
 
-sock.sendto(message.encode('utf-8'), (keys.UDP_IP, keys.UDP_PORT))
+# message = "command"
+# sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+# sock.sendto(message.encode('utf-8'), (keys.UDP_IP, keys.UDP_PORT))
+# time.sleep(2)
+
+tello = Tello(keys.UDP_IP, keys.UDP_PORT)
+tello.send_command("takeoff")
 
