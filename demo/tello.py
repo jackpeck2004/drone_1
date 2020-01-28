c694b0ced14156c09d1f219bd8a9aee258d0f178
Giacomo <giacomo.pasin@gmail.coom>
2020-01-28 14:04:38 +0100

    tried to add recieve into send

--- demo/tello.py
+++ demo/tello.py
@@ -1,1 +1,24 @@
-print("hello")
+import socket
+import time
+
+ip = "drone-001.h-farm.com"
+port = 8889
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+message = "command"
+
+sock.sendto(message.encode('utf-8'), (ip, port))
+
+time.sleep(2)
+
+
+takeoff = "takeoff"
+
+sock.sendto(takeoff.encode('utf-8'), (ip, port))
+
+time.sleep(4)
+
+land = "land"
+
+sock.sendto(land.encode('utf-8'), (ip, port))
